b8af89e39ef7b6825526bbd0a541af06af221f62
josue diaz <hernandzmarlon8@gmail.com>
2024-01-22 20:03:21 -0600

    edit index models

--- database/models/index.js
+++ database/models/index.js
@@ -3,14 +3,11 @@ const { User ,UserModel} = require('../models/UserModel');
 
 function setUpModels(sequelize) {
     Role.init(RoleModel, Role.config(sequelize));
-    User.init(RoleModel, User.config(sequelize));
-
-
+    User.init(UserModel, User.config(sequelize));
 
     // Asociaciones
     Role.associate(sequelize.models);
-    User.associate(sequelize.models)
-
+    User.associate(sequelize.models);
 }
 
 module.exports = { setUpModels };
